e409052c44e4fe520c44bb7e1748767e7685ca0d
dockoS <dockosow97@gmail.com>
2021-01-21 15:31:53 +0000

    qskl

--- my-detection.py
+++ my-detection.py
@@ -1,9 +1,10 @@
 import jetson.inference
 import jetson.utils
-
+from pyimagesearch.centroidtracker import CentroidTracker
 import argparse
 import sys
-
+from collections import OrderedDict
+from matplotlib import pyplot as plt
 # parse the command line
 parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.")
 
@@ -11,7 +12,7 @@ parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of t
 parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
-parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+parser.add_argument("--threshold", type=float, default=0.7, help="minimum detection threshold to use") 
 
 try:
 	opt = parser.parse_known_args()[0]
@@ -19,41 +20,51 @@ except:
 	print("")
 	parser.print_help()
 	sys.exit(0)
-
+ct=CentroidTracker()
 # load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
 # create video sources & outputs
-inputs=jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+variation=[]
+variation1=[]
+
+inputs=jetson.utils.videoSource(opt.input_URI, argv=['--input-codec=h264'])
 output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv)
 compteur=0
 i=0
+data=[]
+labels=[2,3,4,5,6,7,8]
 # process frames until the user exits
 while True:
-
-	# capture the next image
+    # capture the next image
     img = inputs.Capture()
-
-	# detect objects in the image (with overlay)
+    # detect objects in the image (with overlay)
     detections = net.Detect(img, overlay=opt.overlay)
+    rects=[]
     compteur=compteur+len(detections)
-    print("detections ")
-    print(detections)    
-	# print the detections
-    print("detected {:d} objects in image number {:d}".format(len(detections),i))
+    print("detections ={:d}".format(len(detections)))    
+    # print the detections
+    #print("detected {:d} objects in image number {:d}".format(len(detections),i))
     for detection in detections:
-        print(detection)
+        if detection.ClassID in labels:
+            print(" class == {:d}  confiance={:f}". format(detection.ClassID,detection.Confidence))
+            print("center= "+str(detection.Center))
+            rects.append(detection)
+            data.append({"x":detection.Center[0],"y":detection.Center[1]})
+    print("taille rectangle =={:d}".format(len(rects)))
+    # render the image
+    objects=ct.update(rects)
 
-	# render the image
+    #print("objets traques =={:d}".format(len(centroids)))
     output.Render(img)
-
-	# update the title bar
+    # update the title bar
     output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 
-	# print out performance info
+    # print out performance info
     net.PrintProfilerTimes()
-	# exit on input/output EOS
+    # exit on input/output EOS
     i=i+1
     if not inputs.IsStreaming() or not output.IsStreaming():
-    	    break
-print("compteur = {:d}".format(compteur))
+            break
+print("nombre d objet final= {:d}".format(ct.filtrage()))
+
--- pyimagesearch/centroidtracker.py
+++ pyimagesearch/centroidtracker.py
@@ -2,7 +2,13 @@
 from scipy.spatial import distance as dist
 from collections import OrderedDict
 import numpy as np
+import pandas as pd
+from math import sqrt
+import time
+def distance(p):
+    return sqrt(p[0]**2 +p[1]**2)
 
+    
 class CentroidTracker():
 	def __init__(self, maxDisappeared=50):
 		# initialize the next unique object ID along with two ordered
@@ -10,26 +16,44 @@ class CentroidTracker():
 		# ID to its centroid and number of consecutive frames it has
 		# been marked as "disappeared", respectively
 		self.nextObjectID = 0
+		self.variation_rates_centroids=OrderedDict()
 		self.objects = OrderedDict()
 		self.disappeared = OrderedDict()
-
+		self.classes=OrderedDict()
+		self.temps=time.time()
+		self.speed_vectors_dict=OrderedDict()
+		self.speed_vectors=list()
 		# store the number of maximum consecutive frames a given
 		# object is allowed to be marked as "disappeared" until we
 		# need to deregister the object from tracking
 		self.maxDisappeared = maxDisappeared
 
-	def register(self, centroid):
+	def register(self, centroid,Class):
 		# when registering an object we use the next available object
 		# ID to store the centroid
 		self.objects[self.nextObjectID] = centroid
+		self.speed_vectors_dict[self.nextObjectID]=list()
+		self.variation_rates_centroids[self.nextObjectID]=0
 		self.disappeared[self.nextObjectID] = 0
+		self.classes[self.nextObjectID]=Class
 		self.nextObjectID += 1
 
 	def deregister(self, objectID):
 		# to deregister an object ID we delete the object ID from
 		# both of our respective dictionaries
+		
 		del self.objects[objectID]
-		del self.disappeared[objectID]
+		del self.classes[objectID]
+		del self.speed_vectors_dict[objectID]
+	def speed(self,centroidA,centroidB,tB):
+		tA=self.temps
+		vx=0
+		vy=0
+		if tA!=tB :
+			vx=(centroidA[0]-centroidB[0])/(tA-tB)
+			vy=(centroidA[1]-centroidB[1])/(tA-tB)
+		
+		return {"speed":(vx,vy),"time":(tA,tB)}
 
 	def update(self, rects):
 		# check to see if the list of input bounding box rectangles
@@ -43,28 +67,27 @@ class CentroidTracker():
 				# if we have reached a maximum number of consecutive
 				# frames where a given object has been marked as
 				# missing, deregister it
-				if self.disappeared[objectID] > self.maxDisappeared:
-					self.deregister(objectID)
+				#if self.disappeared[objectID] > self.maxDisappeared:
+					#self.deregister(objectID)
 
 			# return early as there are no centroids or tracking info
 			# to update
 			return self.objects
 
 		# initialize an array of input centroids for the current frame
-		inputCentroids = np.zeros((len(rects), 2), dtype="int")
+		inputCentroids = np.zeros((len(rects), 2), dtype="float")
+		inputClasses = np.zeros((len(rects)), dtype="int")
 
 		# loop over the bounding box rectangles
-		for (i, (startX, startY, endX, endY)) in enumerate(rects):
+		for i in range(0,len(rects)):
 			# use the bounding box coordinates to derive the centroid
-			cX = int((startX + endX) / 2.0)
-			cY = int((startY + endY) / 2.0)
-			inputCentroids[i] = (cX, cY)
-
+			inputCentroids[i] = rects[i].Center
+			inputClasses[i]=rects[i].ClassID
 		# if we are currently not tracking any objects take the input
 		# centroids and register each of them
 		if len(self.objects) == 0:
 			for i in range(0, len(inputCentroids)):
-				self.register(inputCentroids[i])
+				self.register(inputCentroids[i],inputClasses[i])
 
 		# otherwise, are are currently tracking objects so we need to
 		# try to match the input centroids to existing object
@@ -100,6 +123,8 @@ class CentroidTracker():
 
 			# loop over the combination of the (row, column) index
 			# tuples
+			#TB is the time now in seconde 
+			tB=time.time()
 			for (row, col) in zip(rows, cols):
 				# if we have already examined either the row or
 				# column value before, ignore it
@@ -111,14 +136,30 @@ class CentroidTracker():
 				# set its new centroid, and reset the disappeared
 				# counter
 				objectID = objectIDs[row]
+				#dans cet partie on calcule le taux de variation du centroide 
+				#la variation de la distance entre le centroid et l origine
+				distance0=distance(self.objects[objectID])
+				distance1=distance(inputCentroids[col])
+				distanceSeries=pd.Series([distance0,distance1])
+				data=distanceSeries.pct_change()[1]
+				print("data")
+				print(data)
+				self.variation_rates_centroids[objectID]=self.variation_rates_centroids[objectID]+data
+				print("variation")			
+				print(self.variation_rates_centroids[objectID])
+				# lets compute the speed of each centroid updated
+				speed_vector=self.speed(self.objects[objectID],inputCentroids[col],tB)
+				#self.speed_vectors.append(speed_vector)
+				self.speed_vectors_dict[objectID].append(speed_vector)
+				#Lets update the centroid value
 				self.objects[objectID] = inputCentroids[col]
 				self.disappeared[objectID] = 0
-
+				
 				# indicate that we have examined each of the row and
 				# column indexes, respectively
 				usedRows.add(row)
 				usedCols.add(col)
-
+			self.temps=tB
 			# compute both the row and column index we have NOT yet
 			# examined
 			unusedRows = set(range(0, D.shape[0])).difference(usedRows)
@@ -139,15 +180,30 @@ class CentroidTracker():
 					# check to see if the number of consecutive
 					# frames the object has been marked "disappeared"
 					# for warrants deregistering the object
-					if self.disappeared[objectID] > self.maxDisappeared:
-						self.deregister(objectID)
+					#if self.disappeared[objectID] > self.maxDisappeared:
+						#self.deregister(objectID)
 
 			# otherwise, if the number of input centroids is greater
 			# than the number of existing object centroids we need to
 			# register each new input centroid as a trackable object
 			else:
-				for col in unusedCols:
-					self.register(inputCentroids[col])
+				for col in unusedCols: 
+					self.register(inputCentroids[col],inputClasses[col])
 
 		# return the set of trackable objects
-		return self.objects
+		return  self.objects,self.variation_rates_centroids
+	def filtrage(self):
+		print("avant")
+		print(self.objects)
+		print(self.variation_rates_centroids)
+		print(self.classes)
+		for i in range(len(self.objects)):
+			if abs(self.variation_rates_centroids[i])<0.1:
+				self.deregister(i)
+		print("Apres")
+		print(self.objects)
+		print(self.classes)
+		print(self.variation_rates_centroids)
+		print("Speed vectors")
+		print(self.speed_vectors_dict)
+		return len(self.objects)
